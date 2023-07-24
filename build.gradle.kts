535453838b96396ea11764e48a2b32207fdf975e
asaken1021 <asaken1021@outlook.com>
2023-07-25 02:15:31 +0900

    [add] add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,21 @@ plugins {
     kotlin("jvm") version "1.9.0"
 }
 
-group = "org.example"
+group = "net.asaken1021"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        setUrl("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 }
 
 tasks.test {
